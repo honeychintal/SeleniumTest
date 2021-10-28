c0ae9cc48cf225a23391da6df5c8b09134f05631
honey <honeychintal@gmail.com>
2021-10-28 20:31:35 +0530

    commit all things 2nd commit

--- Selenium1/src/test/java/seleniumTests/Day2.java
+++ Selenium1/src/test/java/seleniumTests/Day2.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 
 public class Day2 {
@@ -27,7 +29,7 @@ public class Day2 {
 	    js.executeScript("scrollBy(0, 4500)");  
 	  }
 	
-	@Test
+	//@Test
 	public void RefreshTest() throws Exception
 	{
 		System.setProperty("webdriver.chrome.driver","E:\\SeleniumTest\\Selenium1\\Webdrivers\\chromedriver.exe"); 
@@ -52,4 +54,24 @@ public class Day2 {
 		Thread.sleep(5000);
 		driver.quit();
 	  }
+	
+	@Test
+	public void KeyboardEvent() throws Exception
+	{
+		System.setProperty("webdriver.chrome.driver","E:\\SeleniumTest\\Selenium1\\Webdrivers\\chromedriver.exe"); 
+	           
+		WebDriver driver =new ChromeDriver();
+		driver.get("https://extendsclass.com/text-compare.html");
+		driver.manage().window().maximize();
+		WebElement texta = driver.findElement(By.xpath("//*[@id=\"dropZone\"]//div[7]/pre/span"));
+		
+		Actions action = new Actions(driver);
+		Thread.sleep(2000);
+		action.keyDown(texta, Keys.CONTROL).sendKeys("a").sendKeys("c").build().perform();
+		
+		WebElement textb = driver.findElement(By.xpath("//*[@id=\"dropZone2\"]//div[5]/pre/span "));
+		Thread.sleep(2000);
+		action.keyDown(textb, Keys.CONTROL).sendKeys("a").sendKeys("v").build().perform();
+		 
+	}
 }
